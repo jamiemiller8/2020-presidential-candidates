53efe058f44e8a4eb1b4840f714fd26ad0d283d1
Jamie Miller <miller0328@gmail.com>
2019-09-10 16:57:08 -0400

    working on button and button css

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import { Link,Route } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
 import "./Home.css";
-
 
 class Home extends Component {
   render() {
@@ -9,30 +8,34 @@ class Home extends Component {
       <div className="home-container">
         <div className="title">
           <h1>The Women of 2020</h1>
-        <p>
-          Welcome to The Women of 2020, a website that informs and keeps you
-          updated on the female candidates running for the 2020 Presidential
-          Election.
-        </p>
+          <p>
+            Welcome to The Women of 2020, a website that informs and keeps you
+            updated on the female candidates running for the 2020 Presidential
+            Election.
+          </p>
         </div>
         <div className="home-links">
-            <div className="home-candidate-link">
-                <Link to="/candidates/" className="link">
-                    <h2>Candidates</h2>
-                </Link>
-            </div>
-            <div className="home-issues-link">
-                <Link to="/issues/" className="link">
-                    <h2>Issues</h2>
-                </Link>
-            </div>
-            <div className="home-form-link">
-                <Link to="/form/" className="link">
-                    <h2>Suggestion Form</h2>
-                </Link>
-            </div>
-
-
+          <div className="home-candidate-link">
+            <button class="btn">
+              <Link to="/candidates/" className="link">
+                <h2>Candidates</h2>
+              </Link>
+            </button>
+          </div>
+          <div className="home-issues-link">
+            <button class="btn">
+              <Link to="/issues/" className="link">
+                <h2>Issues</h2>
+              </Link>
+            </button>
+          </div>
+          <div className="home-form-link">
+            <button class="btn">
+              <Link to="/form/" className="link">
+                <h2>Suggestion Form</h2>
+              </Link>
+            </button>
+          </div>
         </div>
       </div>
     );
