c1c4d6cc1269ec808663b6f6c4595af38f450860
Jamie Miller <miller0328@gmail.com>
2019-09-11 21:26:43 -0400

    experimenting with abortion issue file

--- src/Issue/Abortion.js
+++ src/Issue/Abortion.js
@@ -1,10 +1,63 @@
 import React, { Component } from "react";
+import './Abortion.css';
 
 class Abortion extends Component {
     render() {
         return (
-            <h1>Test</h1>
-            
+            <div className="abortion">
+                <div className="abortion-header">
+                <h1>Abortion</h1>
+                <p>Does the candidate believe in abortions being legal?</p>
+                </div>
+                <div className="abortion-categories">
+                    <div className="sub-head">
+                    <h2>For</h2>
+                    </div>
+                    <ul>
+                        <li>
+                            <h3>
+                                <span>Tulsi Gabbard</span>
+                            </h3>
+                        </li>
+                        <li>
+                            <h3>
+                                <span>Kirsten Gillibrand</span>
+                            </h3>
+                        </li>
+                        <li>
+                            <h3>
+                                <span>Kamala Harris</span>
+                            </h3>
+                        </li>
+                        <li>
+                            <h3>
+                                <span>Elizabeth Warren</span>
+                            </h3>
+                        </li>
+                        <li>
+                            <h3>
+                                <span>Marianne Williamson</span>
+                            </h3>
+                        </li>
+                    </ul>
+                    <div className="sub-head">
+                    <h2>Other</h2>
+                    </div>
+                    <ul>
+                        <li>
+                            <h3>
+                                <span>Amy Klobuchar</span>
+                            </h3>
+                        </li>
+                    </ul>
+                    <div className="sub-head">
+                    <h2>Undecided</h2>
+                    </div>
+                    <div className="sub-head">
+                    <h2>Against</h2>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
