3e713d23485df69ce44046efa1f4bed10b39bd4c
Jamie Miller <miller0328@gmail.com>
2019-09-15 22:03:37 -0400

    deployment issues

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom"
+import * as serviceWorker from "./serviceWorker"
 
 ReactDOM.render(
     <Router>
         <App />
     </Router>,
-    document.getElementById("root"))
+    document.getElementById("root"));
 
+serviceWorker.unregister();
