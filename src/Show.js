7abf0b84c79585377d40fc8e5ef57a77f7e5e470
Jamie Miller <miller0328@gmail.com>
2019-10-01 17:36:50 -0400

    centered candidate video

--- src/Show.js
+++ src/Show.js
@@ -26,8 +26,9 @@ class Show extends Component {
         <img class={"show-image " + disabledClass} src={chosenCandidate[0].image} alt={chosenCandidate.name}></img>
         <p>Status: {runningPresent}</p>
         <p>{chosenCandidate[0].description}</p>
-        <iframe width="560" height="315" src={chosenCandidate[0].socialMedia.youtube} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-
+        <div className="candidate-video">
+        <iframe className="video" width="560" height="315" src={chosenCandidate[0].socialMedia.youtube} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        </div>
         <button  id="show-btn" class="btn">
           <div className="learn-more-btn">
             <a target="_blank" rel="noopener noreferrer" href={chosenCandidate[0].website}>Learn More</a>
